3e6aac20fb94ac3a97b0ec8fbb2d1d7cfd2b63a5
muhammadganny <mohammadganny@gmail.com>
2023-08-22 16:45:28 +0500

    ac

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const app = express()
-const PORT =  4000
+const PORT =  process.env.PORT || 4000
 
 app.use(express.json());
 app.use(cors());
